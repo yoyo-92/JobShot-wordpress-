c6f7f91bacbdcfdfeed8fd2fe2b460fa47669f7b
dfc22 <dolphinf.c.22@softbank.ne.jp>
2019-12-10 13:45:44 +0900

    add

---
+++ google_map_api.php
@@ -0,0 +1,86 @@
+<?php
+
+function get_time_to_station($address){
+    mb_language("Japanese");//文字コードの設定
+    mb_internal_encoding("UTF-8");
+    $api_key='AIzaSyAo-HEaelxKq4jLvmvCg8HI7_UKoGAz_ms';
+    $address = urlencode($address[0]);
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $address . "+&key=" . $api_key ;
+    $contents= file_get_contents($url);
+    $jsonData = json_decode($contents,true);
+
+    $lat = $jsonData["results"][0]["geometry"]["location"]["lat"];
+    $lng = $jsonData["results"][0]["geometry"]["location"]["lng"];
+
+    $stations=array();
+    print($lat.$lng);
+
+    $url1="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".strval($lat).",".strval($lng)."&radius=800&type=train_station&language=ja&key=" . $api_key ;
+    $contents1= file_get_contents($url1);
+    $jsonData1 = json_decode($contents1,true);
+    $lat1 = $jsonData1["results"][0]["geometry"]["location"]["lat"];
+    $lng1 = $jsonData1["results"][0]["geometry"]["location"]["lng"];
+    foreach($jsonData1["results"] as $data){
+        $nameArray = array_column($stations, 'name');
+        $sta=$data["name"];
+        if(!in_array($sta,$nameArray)){
+            $endpoint = 'https://maps.googleapis.com/maps/api/directions/json?language=ja&origin='.strval($lat1).",".strval($lng1).'&mode=walking&destination='.$address.'&key='.$api_key;
+            $contents3= file_get_contents($endpoint);
+            $jsonData3 = json_decode($contents3,true);
+            $sta1 = rtrim($sta, '駅');
+            $get_lines='http://express.heartrails.com/api/json?method=getStations&name='.$sta1;
+            $lines_contents=file_get_contents($get_lines);
+            $lines_json = json_decode($lines_contents,true);
+            $lines=array();
+            foreach($lines_json['response']['station'] as $ln){
+                array_push($lines,$ln['line']);
+            }
+            array_push($stations,array('name'=>$sta,'distance'=>$jsonData3["routes"][0]["legs"][0]["distance"]["text"],'time'=>$jsonData3["routes"][0]["legs"][0]["duration"]["text"],'line'=>$lines));
+        }
+    }
+
+    $url2="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=".strval($lat).",".strval($lng)."&radius=800&type=subway_station&language=ja&key=" . $api_key ;
+    $contents2= file_get_contents($url2);
+    $jsonData2 = json_decode($contents2,true);
+    $lat2 = $jsonData2["results"][0]["geometry"]["location"]["lat"];
+    $lng2 = $jsonData2["results"][0]["geometry"]["location"]["lng"];
+    foreach($jsonData2["results"] as $data){
+        $nameArray = array_column($stations, 'name');
+        $sta=$data["name"];
+        if(!in_array($sta,$nameArray)){
+            $endpoint = 'https://maps.googleapis.com/maps/api/directions/json?language=ja&origin='.strval($lat2).",".strval($lng2).'&mode=walking&destination='.$address.'&key='.$api_key;
+            $contents4= file_get_contents($endpoint);
+            $jsonData4 = json_decode($contents4,true);
+            $sta1 = rtrim($sta, '駅');
+            $get_lines='http://express.heartrails.com/api/json?method=getStations&name='.$sta1;
+            $lines_contents=file_get_contents($get_lines);
+            $lines_json = json_decode($lines_contents,true);
+            $lines=array();
+            foreach($lines_json['response']['station'] as $ln){
+                array_push($lines,$ln['line']);
+            }
+            array_push($stations,array('name'=>$sta,'distance'=>$jsonData4["routes"][0]["legs"][0]["distance"]["text"],'time'=>$jsonData4["routes"][0]["legs"][0]["duration"]["text"],'line'=>$lines));
+        }
+    }
+    return $stations;
+}
+add_shortcode('get_time_to_station','get_time_to_station');
+
+//テスト用
+function delete_station_meta(){
+	$args = array(
+        'post_status' => array('publish'),
+        'post_type' => array('internship'),
+        'order' => 'DESC',
+        'posts_per_page' => -1
+    );
+    $posts = get_posts( $args );
+    $custom_key = 'station';
+    foreach($posts as $post){
+        $post_id=$post->ID;
+        delete_post_meta($post_id, $custom_key);
+    }
+}
+add_shortcode('delete_station_meta','delete_station_meta');
+
+?>
