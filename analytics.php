2f92f5972ddf59178d44cf4209f9761811464483
Takao0429 <takao_lotte7@yahoo.co.jp>
2020-01-12 16:41:03 +0900

    instagramアナリティクス導入

---
+++ analytics.php
@@ -0,0 +1,40 @@
+<?php
+function google_analytics_add_wp_head (){
+    echo "<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-135934453-1'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-135934453-1');
+    </script>";
+
+}
+add_action ('wp_head','google_analytics_add_wp_head',1);
+
+function instagram_analytics_add_wp_head (){
+    echo "
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '866794000437602');
+    fbq('track', 'PageView');
+    </script>
+    <noscript>
+    <img height='1' width='1'
+    src='https://www.facebook.com/tr?id=866794000437602&ev=PageView
+    &noscript=1'/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->";
+
+}
+add_action ('wp_head','instagram_analytics_add_wp_head',1);
+
+?>
